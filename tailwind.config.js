0dbc15f462f49d4add3f46396921ccd9a745bc8d
lamyi9 <lamyig2022@gmail.com>
2025-01-07 14:12:09 +0530

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
@@ -7,10 +8,16 @@ export default {
         brownDark: "#944343",
         brown : "#c55959",
         brownLight : "#d48383",
+        flowbiteTab: {
+          active: '#D61F69', // Custom active color
+          inactive: '#0E9F6E', // Custom inactive color
+          focus: '#C27803',
+        },
       },
       fontFamily:{
         'bo': ['bo']
-      }
+      },
+      
     },
    
   },
