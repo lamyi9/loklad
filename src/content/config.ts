10855ca34d38e0371b827e0859d5dc1038d2e18e
lamyi9 <lamyig2022@gmail.com>
2025-01-13 11:17:10 +0530

    added images

--- src/content/config.ts
+++ src/content/config.ts
@@ -43,6 +43,27 @@ const browserCollection = defineCollection({
 });
 
 // Define a collection of Apps
+const comCollection = defineCollection({
+    type: 'content', 
+// Define the schema 
+    schema:({image})=> z.object({
+        title: z.string().max(100),
+        category: z.array(z.string()),
+        summary: z.string(),
+        banner: image().optional(), //Image can be optional
+        thumbnail: image(),
+        author: z.string(),
+        website: z.string().url(),
+        collection: z.string().url(),
+        country: z.string(),
+        rating: z.number(),
+        apk: z.string().url().optional(),
+        windows: z.string().url().optional(),
+        apple: z.string().url().optional(),
+    }),
+});
+
+// Define a collection of Apps
 const toolsCollection = defineCollection({
     type: 'content', 
 // Define the schema 
@@ -61,13 +82,12 @@ const toolsCollection = defineCollection({
         windows: z.string().url().optional(),
         apple: z.string().url().optional(),
     }),
-    
-
 });
 // Export
 
 export const collections = {
     'apps': 'appCollection',
     'browsers': 'browserCollection',
+    'communication': 'comCollection',
     'tools': 'toolsCollection',
 };
