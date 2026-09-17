9925bba21f2d4796bd9c6ef710d0e098837dfa39
tenzintai <tyega@tibetaction.net>
2026-09-17 11:33:32 +0530

    fixing deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-
-    integrations: [tailwind(), mdx()],
-    // content: {
-    //     collections: {
-    //         apps:{},
-    //         browsers:{},
-    //         tools:{},
-    //     }
-    // }
-    adapter: cloudflare(), 
+    integrations: [mdx()],
+    adapter: cloudflare(),
     output: "server",
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
